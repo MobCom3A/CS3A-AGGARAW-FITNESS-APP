786550c9ba852999371c292c814b310e1ff329dd
MobCom3A <mobilecomputingbscs3a@gmail.com>
2023-06-19 11:42:24 +0800

    first commit

---
+++ fitness-app/lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:workout_app/screens/welcom_view.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomView(),
+    );
+  }
+}
